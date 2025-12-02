5a5322e4bbb79edd701a98568e0e2a431ab46d00
thomas.grandjean <tgrandje@gmail.com>
2025-12-02 17:40:40 +0100

    update dataset

--- src/hackathon_climat_donnees/prep_datasets.py
+++ src/hackathon_climat_donnees/prep_datasets.py
@@ -4,9 +4,6 @@ Extraction des données sur les installations classées depuis Géorisques
 """
 
 import geopandas as gpd
-import pandas as pd
-
-from french_cities import find_city
 
 URL = "https://mapsref.brgm.fr/wxs/georisques/georisques_dl?&service=wfs&version=2.0.0&request=getfeature&typename=InstallationsClassees&outputformat=SHAPEZIP"
 
@@ -29,7 +26,14 @@ def prepare_dataset():
     gdf = gdf[gdf.ied == 1]
     gdf = gdf.drop("ied", axis=1)
 
+    rubriques = ["rubriques_", "rubrique_1", "rubrique_2"]
+    gdf["rubrique"] = gdf[rubriques].fillna("").agg("|".join, axis=1)
+    gdf = gdf.drop(rubriques, axis=1)
+
+    # export multi-format
     gdf.to_file("sample.gpkg", driver="GPKG")
+    gdf.to_file("sample.shp", driver="GPKG")
+    gdf.drop("geometry", axis=1).to_csv("sample.csv", sep=";")
 
     return gdf
 
