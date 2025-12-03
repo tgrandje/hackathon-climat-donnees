88b1f858c003171f01ca465bdc1fabc5980d501d
thomas.grandjean <tgrandje@gmail.com>
2025-12-03 11:13:07 +0100

    update dataset with natural hazards

--- src/hackathon_climat_donnees/prep_datasets.py
+++ src/hackathon_climat_donnees/prep_datasets.py
@@ -5,18 +5,25 @@ Extraction des données sur les installations classées depuis Géorisques
 
 import io
 import logging
-from functools import lru_cache
+import os
 from zipfile import ZipFile
 
 import geopandas as gpd
 import pandas as pd
 from requests_cache import CachedSession
+from tqdm import tqdm
 
 from hackathon_climat_donnees.constants import GEREP_THRESHOLDS
 
 logger = logging.getLogger(__name__)
 
-SESSION = CachedSession("cache", expire_after=60 * 60 * 24 * 10)
+CACHE_DURATION_SECONDS = 60 * 60 * 24 * 10
+SESSION = CachedSession(
+    "cache",
+    backend="sqlite",
+    expire_after=CACHE_DURATION_SECONDS,
+    allowable_methods=("GET", "POST"),
+)
 
 GEORISQUES_SERVICES = "https://www.georisques.gouv.fr/themes/custom/georisques/assets/dist/js/georisques_commun/web-service-urls.json"
 webservices = SESSION.get(GEORISQUES_SERVICES).json()
@@ -25,14 +32,31 @@ download_url = webservices["DOWNLOAD"]
 
 def to_disk(gdf: gpd.GeoDataFrame) -> None:
     # export multi-format
-    gdf.to_file("sample.gpkg", driver="GPKG")
-    gdf.to_file("sample.shp")
+    os.makedirs("./output")
+    gdf.to_file("./output/sample.gpkg", driver="GPKG")
+    gdf.to_file("./output/sample.shp")
     gdf = gdf.copy()
-    gdf.drop("geometry", axis=1).to_csv("sample.csv", sep=";")
-    gdf.to_file("sample.geojson", driver="GeoJSON")
+    gdf.drop("geometry", axis=1).to_csv("./output/sample.csv", sep=";")
+    gdf.to_file("./output/sample.geojson", driver="GeoJSON")
 
 
 def prepare_dataset() -> gpd.GeoDataFrame:
+    """
+    Identification des principales ICPE métropolitaines. Utilise la base
+    de données "Installations industrielles" de Géorisques
+
+    La sélection opérée est :
+        * SEVESO seuil haut
+        * classé en priorité nationale
+        * en métropole
+        * soumis à la directive IED
+
+    Returns
+    -------
+    gdf : gpd.GeoDataFrame
+        GeoDataFrame des principales ICPE métropolitaines
+
+    """
 
     files = SESSION.get(download_url + "/icpe", data={"annemin": 2003}).json()
     url = files["national"]["lien"]
@@ -61,7 +85,66 @@ def prepare_dataset() -> gpd.GeoDataFrame:
     return gdf
 
 
-def profile_irep() -> dict:
+def profile_irep() -> gpd.GeoDataFrame:
+    """
+    Evalue des "profils" des ICPE à partir des données IREP de Géorisques :
+    préleveur majeur, volume rejeté important, fortes émissions atmosphériques,
+    ...
+
+    Permet d'évaluer en première approche les sujets prioritaires pour les ICPE
+    les plus impactantes de France.
+
+    Les calculs sont faits en prenant en compte les émissions médianes
+    déclarées sur les 5 dernières années pour chaque établissement, puis
+    (généralement) en sélectionnant le percentile 75 de la métrique considérée.
+
+    * prelevements en eau (lien avec la réduction des volumes disponibles):
+      en première approximation, on définit un préleveur majeur comme une
+      ICPE qui se situe dans le 1er quartile annuel, hors prélèvements en
+      mer
+
+    * rejets en eau (lien potentiel avec le rejet de chaleur dans des cours
+      d'eau déjà réchauffés)
+      en première approximation, on définit un rejet majeur comme une ICPE
+      qui se situe dans le 1er quartile annuel, hors rejets en réseau
+
+    * rejets atmosphériques (lien avec l'altération du vent)
+      calcul d'un score environnemental approximatif (serait à améliorer pour
+      ne prendre en compte que les substances à réel impact sanitaire) puis
+      sélection du top 25%
+
+    * rejets aqueux (lien avec la réduction des débits d'étiage, entraînant
+      une augmentation de l'impact des rejets avec un facteur de dilution
+      moindre)
+      calcul d'un score environnemental parmi les rejets directs au milieu (par
+      opposition aux rejets en réseau d'assainissement) puis sélection du top 25%
+
+    * rejets sur les sols = épandages (lien avec le changement des
+      précipitations pouvant impacter percolation et ruissellement)
+      calcul d'un score environnemental puis sélection du top 25%
+
+    Returns
+    -------
+    gpd.GeoDataFrame
+        Données IREP des ICPE
+        Ex:
+            identifiant                               nom_etablissement    numero_siret  \
+          0  0000000039  COLLECTES VALORISATION ENERGIE DECHETS - COVED  34340353103542   
+          1  0003012003                                        NOVAWOOD  82181865500010   
+
+                                                       adresse code_insee  \
+          0  AVENUE DES EOLIENNES   LE RAZAS 26780 MALATAVERNE      26169   
+          1  chemin du Vaquené 54410 LANEUVEVILLE-DEVANT-NANCY      54300   
+
+            code_departement code_region                        geometry PLV_Q75  \
+          0              26           84  POINT (839475.211 6374347.037)   False   
+          1              54           44  POINT (940538.885 6842661.186)   False   
+
+            VOLREJ_Q75 AIR_Q75 EAU_Q75 SOL_Q75  
+          0      False   False   False   False  
+          1      False    True   False   False  
+
+    """
 
     files = SESSION.get(download_url + "/irep", data={"annemin": 2003}).json()
     seek = ["etablissements", "emissions", "prelevements", "rejets"]
@@ -132,7 +215,7 @@ def profile_irep() -> dict:
             index=ix,
         ).to_crs(2154)
         df.loc[ix, "geometry"] = geoms
-    df = gpd.GeoDataFrame(df)
+    df = gpd.GeoDataFrame(df, crs=2154)
     df = df.drop(["coordonnees_x", "coordonnees_y"], axis=1)
     df = df[
         [
@@ -167,7 +250,7 @@ def profile_irep() -> dict:
     df = df.groupby("identifiant")["prelevement"].median()
     q75 = df.quantile(0.75)
     df = df > q75
-    df = df.to_frame("top25% prelevements").reset_index(drop=False)
+    df = df.to_frame("PLV_Q75").reset_index(drop=False)
     dict_df["prelevements"] = df
 
     # prétraitement des données rejets
@@ -180,7 +263,7 @@ def profile_irep() -> dict:
     df = df.groupby("identifiant")["rejet_isole_m3_par_an"].median()
     q75 = df.quantile(0.75)
     df = df > q75
-    df = df.to_frame("top25% rejets").reset_index(drop=False)
+    df = df.to_frame("VOLREJ_Q75").reset_index(drop=False)
     dict_df["rejets"] = df
 
     # prétraitement des données émissions air : calcul d'un score
@@ -203,7 +286,7 @@ def profile_irep() -> dict:
     )
     q75 = df.quantile(0.75)
     df = df > q75
-    df = df.to_frame("top25% rejets atmo").reset_index(drop=False)
+    df = df.to_frame("AIR_Q75").reset_index(drop=False)
     dict_df["emissions_air"] = df
 
     # eau, rejet direct au milieu uniquement
@@ -230,7 +313,7 @@ def profile_irep() -> dict:
     )
     q75 = df.quantile(0.75)
     df = df > q75
-    df = df.to_frame("top25% rejets aqueux").reset_index(drop=False)
+    df = df.to_frame("EAU_Q75").reset_index(drop=False)
     dict_df["emissions_eau"] = df
 
     # sols
@@ -253,30 +336,28 @@ def profile_irep() -> dict:
     )
     q75 = df.quantile(0.75)
     df = df > q75
-    df = df.to_frame("top25% rejets sol").reset_index(drop=False)
+    df = df.to_frame("SOL_Q75").reset_index(drop=False)
     dict_df["emissions_sol"] = df
 
     del dict_df["emissions"]
 
-    return dict_df
+    etabs = dict_df.pop("etablissements")
+    for key, meta in dict_df.items():
+        etabs = etabs.merge(meta, on="identifiant", how="left")
+
+    for col in ["PLV_Q75", "VOLREJ_Q75", "AIR_Q75", "EAU_Q75", "SOL_Q75"]:
+        ix = etabs[etabs[col].isnull()].index
+        etabs.loc[ix, col] = False
+
+    return etabs
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    gdf = prepare_dataset()
-    profiles = profile_irep()
+def merge_datasets(gdf, irep_profiles) -> gpd.GeoDataFrame:
+    # TODO : docstring
     gdf = gdf.rename({"geometry": "geometry_icpe"}, axis=1)
-    for key, meta in profiles.items():
-        gdf = gdf.merge(
-            meta, left_on="code_aiot", right_on="identifiant", how="left"
-        ).drop("identifiant", axis=1)
-    cols = [
-        "top25% prelevements",
-        "top25% rejets",
-        "top25% rejets atmo",
-        "top25% rejets aqueux",
-        "top25% rejets sol",
-    ]
+    gdf = gdf.merge(
+        irep_profiles, left_on="code_aiot", right_on="identifiant", how="left"
+    ).drop("identifiant", axis=1)
 
     # Prendre la meilleure des 2 géométries au regard de la distance
     # entre les deux : si gros écart, prendre la valeur tirée du jeu ICPE
@@ -293,6 +374,142 @@ if __name__ == "__main__":
         .combine_first(gdf.loc[ix, "geometry_icpe"].to_crs(2154))
     )
     gdf = gdf.drop("geometry_icpe", axis=1)
-    for col in cols:
-        gdf[col] = gdf[col].fillna(False)
+
+    # prendre l'adresse et le code commune, dep, region, noms déclarés dans IREP
+    gdf = gdf.drop(["adresse_x", "num_dep", "cd_insee", "nom_ets"], axis=1)
+    gdf = gdf.rename({"adresse_y": "adresse"}, axis=1)
+
+    # Laisser de côté les ICPE pour lesquelles on n'a pas de géolocalisation
+    gdf = gdf[~gdf.geometry.isnull()]
+    gdf = gdf.set_crs(2154)
+
+    # Réévaluer x & y à partir des géométries
+    gdf["x"] = gdf.geometry.x
+    gdf["y"] = gdf.geometry.x
+    gdf["code_epsg"] = 2154
+
+    # Renommer pour éviter les problèmes de troncature au format shp
+    gdf = gdf.rename(
+        {
+            "nom_etablissement": "nom",
+            "numero_siret": "siret",
+            "code_departement": "dept",
+            "code_region": "region",
+        },
+        axis=1,
+    )
+
+    return gdf
+
+
+def hazards(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """
+    Récupère les risques identifiés aux coordonnées des ICPE.
+    Utilise l'API "Rapport PDF et JSON" de géorisques, résultats similaires
+    au rapport "risques près de chez moi".
+
+    Parameters
+    ----------
+    gdf : gpd.GeoDataFrame
+        GeoDataFrame des ICPE.
+
+    Returns
+    -------
+    gdf : gpd.GeoDataFrame
+        GeoDataFrame des risques retournés par l'API
+
+        Encodage des niveaux de risques selon le dictionnaire suivant :
+        {
+            "Risque Inconnu": 1,
+            "Risque Existant": 2,
+            "Risque Existant - faible": 3,
+            "Risque Existant - modéré": 4,
+            "Risque Existant - important": 5,
+        }
+        
+        Ex.:
+            libelle     Avalanche  Feu de foret  Inondation  Recul du trait de cote  \
+            code_aiot                                                                 
+            0003205293          0             0           0                       0   
+            0003300469          0             0           0                       0   
+
+            libelle     Remontée de nappe  Retrait gonflement des argiles  \
+            code_aiot                                                       
+            0003205293                  1                               3   
+            0003300469                  2                               4   
+
+            libelle     Risques côtiers (submersion marine, tsunami)  
+            code_aiot                                                 
+            0003205293                                             0  
+            0003300469                                             0  
+
+    """
+    endpoint = "https://georisques.gouv.fr/api/v1/resultats_rapport_risque"
+    gdf = gdf.to_crs(4326)
+
+    data = []
+    for site in tqdm(gdf.itertuples(False), total=len(gdf)):
+        x = site.geometry.x
+        y = site.geometry.y
+        lonlat = f"{x},{y}"
+        r = SESSION.get(endpoint, params={"latlon": lonlat})
+        natural_hazard = [
+            detail
+            for haz, detail in r.json()["risquesNaturels"].items()
+            if detail.pop("present")
+        ]
+        [d.update({"code_aiot": site.code_aiot}) for d in natural_hazard]
+        data += natural_hazard
+    data = pd.DataFrame(data)
+
+    encoding = {
+        "Risque Inconnu": 1,
+        "Risque Existant": 2,
+        "Risque Existant - faible": 3,
+        "Risque Existant - modéré": 4,
+        "Risque Existant - important": 5,
+    }
+    data["libelleStatutAdresse"] = data["libelleStatutAdresse"].map(encoding)
+    data = (
+        data.pivot_table(
+            "libelleStatutAdresse",
+            columns="libelle",
+            index="code_aiot",
+            aggfunc="sum",
+        )
+        .fillna(0)
+        .astype(int)
+    )
+    for col in data.columns:
+        if (data[col] == 0).all():
+            data = data.drop(col, axis=1)
+
+    # filtrer les risques climatiques liés aux conditions météo
+    drop = ["Mouvements de terrain", "Radon", "Séisme"]
+    data = data.drop(drop, axis=1)
+    return data
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    gdf = prepare_dataset()
+    irep_profiles = profile_irep()
+    gdf = merge_datasets(gdf, irep_profiles)
+
+    # Faire le calcul des risques sur les géométries déclarées dans IREP
+    hazards = hazards(gdf)
+    gdf = gdf.merge(hazards, on="code_aiot", how="left")
+
+    # renommage pour éviter les troncatures SHP
+    gdf = gdf.rename(
+        {
+            "Feu de foret": "feu foret",
+            "Recul du trait de cote": "recul tdc",
+            "Remontée de nappe": "in. nappe",
+            "Retrait gonflement des argiles": "argiles",
+            "Risques côtiers (submersion marine, tsunami)": "risq. cot.",
+        },
+        axis=1,
+    )
+
     to_disk(gdf)
