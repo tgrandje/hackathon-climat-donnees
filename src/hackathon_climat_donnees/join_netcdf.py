663aad72d8fb01830e840faf291e664160f852cd
thomas.grandjean <tgrandje@gmail.com>
2025-12-03 17:34:16 +0100

    refacto netcdf/sample merge

--- src/hackathon_climat_donnees/join_netcdf.py
+++ src/hackathon_climat_donnees/join_netcdf.py
@@ -4,6 +4,7 @@
 Jointure des fichiers netcdf et icpe
 """
 import geopandas as gpd
+import pandas as pd
 import xarray as xr
 
 from hackathon_climat_donnees.prep_datasets import prep_dataset_icpe
@@ -77,10 +78,73 @@ def join_dataset_meteo(
     return gdf
 
 
+def all_scenarii(
+    gdf: gpd.GeoDataFrame, list_paths_netcdf: list[str]
+) -> pd.DataFrame:
+    """
+    Calcul des scenarii pour chaque ICPE
+
+    Parameters
+    ----------
+    gdf : gpd.GeoDataFrame
+        GeoDataFrame des ICPE considérées
+    list_paths_netcdf : list[str]
+        Liste des chemins correspondant à chaque scenario netcdf produit par
+        netcdf_processing
+
+    Raises
+    ------
+    ValueError
+        Dans le cas où un fichier netcdf n'est pas trouvé.
+
+    Returns
+    -------
+    df : pd.DataFrame
+    
+    Ex.:
+                                                  2          5         10  \
+    code_aiot  scenario                                                     
+    0003205293 tasmax_RP_GEV_TRACC2-7.nc  33.745651  35.813251  37.049598   
+               tasmax_RP_GEV_TRACC2.nc    32.628636  34.973529  36.354923   
+    0003300469 tasmax_RP_GEV_TRACC2-7.nc  33.745651  35.813251  37.049598   
+               tasmax_RP_GEV_TRACC2.nc    32.628636  34.973529  36.354923   
+    
+                                                 20         50        100  
+    code_aiot  scenario                                                    
+    0003205293 tasmax_RP_GEV_TRACC2-7.nc  38.145171  39.442737  40.333152  
+               tasmax_RP_GEV_TRACC2.nc    37.564950  38.979659  39.938044  
+    0003300469 tasmax_RP_GEV_TRACC2-7.nc  38.145171  39.442737  40.333152  
+               tasmax_RP_GEV_TRACC2.nc    37.564950  38.979659  39.938044  
+
+    """
+    all_gdfs = []
+    for path in list_paths_netcdf:
+        if not os.path.exists(path):
+            raise ValueError("file not found at %s", path)
+        filename = os.path.basename(path)
+        gdf_with_scenario = join_dataset_meteo(gdf, path)
+        gdf_with_scenario["scenario"] = filename
+        all_gdfs.append(gdf_with_scenario)
+
+    gdf = gpd.pd.concat(all_gdfs)
+
+    cols = [2, 5, 10, 20, 50, 100]
+    df = gdf.set_index(["code_aiot", "scenario"])[cols]
+    df = df.rename(columns={x: str(x) for x in cols})
+    df = df.sort_index()
+    return df
+
+
 # if __name__ == "__main__":
 #     from hackathon_climat_donnees import INPUT
 #     import os
 
 #     gdf = prep_dataset_icpe()
-#     meteo = os.path.join(INPUT, "tasmax_RP_GEV_TRACC2-7.nc")
-#     test = join_dataset_meteo(gdf, meteo)
+
+#     test = all_scenarii(
+#         gdf,
+#         [
+#             os.path.join(INPUT, "tasmax_RP_GEV_TRACC2.nc"),
+#             os.path.join(INPUT, "tasmax_RP_GEV_TRACC2-7.nc"),
+#         ],
+#     )
